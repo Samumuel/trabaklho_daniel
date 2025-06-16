24d9a296b833911062b5997066f0ad76673a1c24
Samuel Profeta <134728703+Samumuel@users.noreply.github.com>
2025-06-16 02:13:55 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,10 +1,9 @@
--- Active: 1726102402957@@127.0.0.1@3306@pokemon
 CREATE TABLE pokemon (
     id int AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(10),
-    tipo VARCHAR(10),
-    estagio VARCHAR(1),
-    hp VARCHAR(4),
-    ataque VARCHAR(4),
-    defesa VARCHAR(4)
+    descricao VARCHAR(1000),
+    link VARCHAR(500),
+    tipo1 VARCHAR(2),
+    tipo2 VARCHAR(2),
+    evolucao VARCHAR(1)
 );
