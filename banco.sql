2741d494e22929adcc006992a2572784cabbd84f
Samuel Profeta <134728703+Samumuel@users.noreply.github.com>
2025-06-27 14:35:40 -0300

    Delete banco.sql

--- banco.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE pokemon (
-    id int AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(10),
-    descricao VARCHAR(1000),
-    link VARCHAR(500),
-    tipo1 VARCHAR(10),
-    tipo2 VARCHAR(10),
-    evolucao VARCHAR(1)
-);
